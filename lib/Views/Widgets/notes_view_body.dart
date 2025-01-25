2379a1caab0e69e1f1fe830302bb55fd6783899b
Abdullah Ayman <abdulluhayman@gmail.com>
2025-01-25 17:32:37 +0200

    Added the design of the notes

--- lib/Views/Widgets/notes_view_body.dart
+++ lib/Views/Widgets/notes_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:to_do_list/Views/Widgets/custom_app_bar.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -11,7 +12,86 @@ class NotesViewBody extends StatelessWidget {
       child: Column(
         children: [
           CustomAppBar(),
+          // ListView.builder(
+          //   itemBuilder: (context, index) {
+          //     return NoteItem();
+          //   },
+          //   itemCount: 2,
+          // ),
+          NoteItem(),
         ],
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xffffcc80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(
+          top: 24,
+          bottom: 24,
+          left: 16,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                "Flutter Tips",
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                  bottom: 16,
+                ),
+                child: Text(
+                  "Build your career with Tharwat Samy",
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.5),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  // Icons.delete,
+                  color: Colors.black,
+                  size: 24,
+                ),
+              ),
+            ),
+            const Opacity(
+              opacity: 0.4,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  right: 24,
+                ),
+                child: const Text(
+                  "2023-15-2",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:to_do_list/Views/note_view.dart';
 
 void main() {
@@ -12,6 +13,9 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
         brightness: Brightness.dark,
       ),
       debugShowCheckedModeBanner: false,
